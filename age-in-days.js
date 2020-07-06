dc085fa46528bed07cac1f0eb57dcfe99f41094a
BV Satyaram <bvsatyaram@gmail.com>
2020-07-06 06:25:56 +0530

    Bugfix: our has 3600 seconds, not 60

--- age-in-days.js
+++ age-in-days.js
@@ -1,7 +1,7 @@
 var currentTime = new Date();
 var dateOfBirth = new Date("2000-01-01");
 var age = currentTime - dateOfBirth;
-age = age / (24 * 60 * 1000);
+age = age / (24 * 60 * 60 * 1000);
 age = Math.round(age);
 
 document.querySelector("h3").innerHTML = age;
